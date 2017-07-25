81a80bb2600e26f1d8d9a6d20d8e678d3dce5d35
Emerson Contiero <enconti@gmail.com>
2017-07-25 15:09:24 +0000

    Atualização

--- project/__init__.py
+++ project/__init__.py
@@ -1,18 +1,29 @@
 # project/__init__.py
 
+
+import os
 from flask import Flask, jsonify
-
-#i instantiate the app
-app = Flask(__name__)
-
-# set config
-app.config.from_object('project.config.DevelopmentConfig')
+from flask_sqlalchemy import SQLAlchemy
 
 
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+# instantiate the db
+db = SQLAlchemy()
 
+
+def create_app():
+
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.views import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    return app
